28b1cae5cf9bb4ce2dff310a34b835d9dbcc8002
Schika <we19989@mail.ru>
2018-01-17 16:31:03 +0300

    Создание примитивного чека

--- FW16AutoTestProgram/FW16AutoTestProgram/Form1.cs
+++ FW16AutoTestProgram/FW16AutoTestProgram/Form1.cs
@@ -31,11 +31,22 @@ namespace FW16AutoTestProgram
             catch (EcrException excep)
             {
                 ecrCtrl.Reconnect();
+                System.Diagnostics.Debug.Write(excep.Message);
             }
             catch(System.IO.IOException excep)
             {
                 MessageBox.Show(excep.Message);
             }
+            CreateCheck();
+        }
+        private void CreateCheck()
+        {
+            //ecrCtrl.Shift.Open();
+            var receipt = ecrCtrl.Shift.BeginReceipt("Касир", Fw16.Model.ReceiptKind.Income);
+            receipt.AddEntry(receipt.NewItemPriced("1", "Товар 1", Native.CmdExecutor.VatCodeType.Vat18, 10.50m, 0.5m));
+            receipt.AddPayment(Native.CmdExecutor.TenderCode.Cash, 10.25m);
+            receipt.Complete();
+
         }
     }
 }
