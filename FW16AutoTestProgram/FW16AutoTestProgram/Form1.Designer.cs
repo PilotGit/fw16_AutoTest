97dd48b2df1712631a66907d249c505c481f0873
Schika <we19989@mail.ru>
2018-01-19 10:35:40 +0300

    попытка закрыть смену дважды

--- FW16AutoTestProgram/FW16AutoTestProgram/Form1.Designer.cs
+++ FW16AutoTestProgram/FW16AutoTestProgram/Form1.Designer.cs
@@ -32,42 +32,19 @@
             this.RateCB = new System.Windows.Forms.ComboBox();
             this.PortCB = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label_id = new System.Windows.Forms.Label();
             this.label_version = new System.Windows.Forms.Label();
             this.label_firmware = new System.Windows.Forms.Label();
             this.label_stats_connect = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.startValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.finishValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.chekValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.startValueS2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.finishValueS2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.checkValueS2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.label_id = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // Connect
             // 
-            this.Connect.Location = new System.Drawing.Point(621, 12);
+            this.Connect.Location = new System.Drawing.Point(267, 12);
             this.Connect.Name = "Connect";
             this.Connect.Size = new System.Drawing.Size(108, 23);
             this.Connect.TabIndex = 0;
@@ -78,7 +55,7 @@
             // RateCB
             // 
             this.RateCB.FormattingEnabled = true;
-            this.RateCB.Location = new System.Drawing.Point(481, 14);
+            this.RateCB.Location = new System.Drawing.Point(140, 12);
             this.RateCB.Name = "RateCB";
             this.RateCB.Size = new System.Drawing.Size(121, 21);
             this.RateCB.TabIndex = 1;
@@ -86,7 +63,7 @@
             // PortCB
             // 
             this.PortCB.FormattingEnabled = true;
-            this.PortCB.Location = new System.Drawing.Point(354, 14);
+            this.PortCB.Location = new System.Drawing.Point(13, 12);
             this.PortCB.Name = "PortCB";
             this.PortCB.Size = new System.Drawing.Size(121, 21);
             this.PortCB.TabIndex = 2;
@@ -99,11 +76,19 @@
             this.groupBox1.Controls.Add(this.label_stats_connect);
             this.groupBox1.Location = new System.Drawing.Point(13, 41);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(270, 539);
+            this.groupBox1.Size = new System.Drawing.Size(270, 537);
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Данные о ККТ";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label_id
+            // 
+            this.label_id.AutoSize = true;
+            this.label_id.Font = new System.Drawing.Font("Lucida Bright", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_id.Location = new System.Drawing.Point(6, 96);
+            this.label_id.Name = "label_id";
+            this.label_id.Size = new System.Drawing.Size(0, 17);
+            this.label_id.TabIndex = 3;
             // 
             // label_version
             // 
@@ -133,210 +118,39 @@
             this.label_stats_connect.TabIndex = 0;
             this.label_stats_connect.Text = "ККТ: не подключено";
             // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.dataGridView2);
-            this.groupBox2.Controls.Add(this.label2);
-            this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Controls.Add(this.dataGridView1);
-            this.groupBox2.Location = new System.Drawing.Point(289, 41);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(893, 412);
-            this.groupBox2.TabIndex = 4;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Тестирование";
-            // 
-            // dataGridView2
-            // 
-            this.dataGridView2.AllowUserToAddRows = false;
-            this.dataGridView2.AllowUserToDeleteRows = false;
-            this.dataGridView2.AllowUserToResizeRows = false;
-            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.dataGridViewTextBoxColumn1,
-            this.dataGridViewTextBoxColumn2,
-            this.dataGridViewTextBoxColumn3,
-            this.dataGridViewTextBoxColumn4,
-            this.dataGridViewTextBoxColumn5,
-            this.dataGridViewTextBoxColumn6,
-            this.dataGridViewTextBoxColumn7,
-            this.dataGridViewTextBoxColumn8});
-            this.dataGridView2.Location = new System.Drawing.Point(454, 38);
-            this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.RowHeadersVisible = false;
-            this.dataGridView2.Size = new System.Drawing.Size(433, 368);
-            this.dataGridView2.TabIndex = 4;
-            // 
-            // dataGridViewTextBoxColumn1
-            // 
-            this.dataGridViewTextBoxColumn1.HeaderText = "id";
-            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
-            this.dataGridViewTextBoxColumn1.ReadOnly = true;
-            this.dataGridViewTextBoxColumn1.Width = 30;
-            // 
-            // dataGridViewTextBoxColumn2
-            // 
-            this.dataGridViewTextBoxColumn2.HeaderText = "name";
-            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
-            this.dataGridViewTextBoxColumn2.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn3
-            // 
-            this.dataGridViewTextBoxColumn3.HeaderText = "startValueS1";
-            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
-            this.dataGridViewTextBoxColumn3.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn4
-            // 
-            this.dataGridViewTextBoxColumn4.HeaderText = "finishValueS1";
-            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
-            this.dataGridViewTextBoxColumn4.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn5
-            // 
-            this.dataGridViewTextBoxColumn5.HeaderText = "checkValueS1";
-            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
-            this.dataGridViewTextBoxColumn5.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn6
-            // 
-            this.dataGridViewTextBoxColumn6.HeaderText = "startValueS2";
-            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
-            // 
-            // dataGridViewTextBoxColumn7
-            // 
-            this.dataGridViewTextBoxColumn7.HeaderText = "finishValueS2";
-            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
-            this.dataGridViewTextBoxColumn7.ReadOnly = true;
-            // 
-            // dataGridViewTextBoxColumn8
-            // 
-            this.dataGridViewTextBoxColumn8.HeaderText = "checkValueS2";
-            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
-            this.dataGridViewTextBoxColumn8.ReadOnly = true;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(451, 19);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AllowUserToResizeRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.id,
-            this.name,
-            this.startValue,
-            this.finishValue,
-            this.chekValue,
-            this.startValueS2,
-            this.finishValueS2,
-            this.checkValueS2});
-            this.dataGridView1.Location = new System.Drawing.Point(7, 38);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.Size = new System.Drawing.Size(433, 368);
-            this.dataGridView1.TabIndex = 0;
-            // 
-            // id
-            // 
-            this.id.HeaderText = "id";
-            this.id.Name = "id";
-            this.id.ReadOnly = true;
-            this.id.Width = 30;
-            // 
-            // name
-            // 
-            this.name.HeaderText = "name";
-            this.name.Name = "name";
-            this.name.ReadOnly = true;
-            // 
-            // startValue
-            // 
-            this.startValue.HeaderText = "startValueS1";
-            this.startValue.Name = "startValue";
-            this.startValue.ReadOnly = true;
-            // 
-            // finishValue
-            // 
-            this.finishValue.HeaderText = "finishValueS1";
-            this.finishValue.Name = "finishValue";
-            this.finishValue.ReadOnly = true;
-            // 
-            // chekValue
-            // 
-            this.chekValue.HeaderText = "checkValueS1";
-            this.chekValue.Name = "chekValue";
-            this.chekValue.ReadOnly = true;
-            // 
-            // startValueS2
-            // 
-            this.startValueS2.HeaderText = "startValueS2";
-            this.startValueS2.Name = "startValueS2";
-            // 
-            // finishValueS2
-            // 
-            this.finishValueS2.HeaderText = "finishValueS2";
-            this.finishValueS2.Name = "finishValueS2";
-            this.finishValueS2.ReadOnly = true;
-            // 
-            // checkValueS2
-            // 
-            this.checkValueS2.HeaderText = "checkValueS2";
-            this.checkValueS2.Name = "checkValueS2";
-            this.checkValueS2.ReadOnly = true;
-            // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(289, 459);
+            this.listBox1.Location = new System.Drawing.Point(289, 41);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(893, 121);
+            this.listBox1.Size = new System.Drawing.Size(893, 537);
             this.listBox1.TabIndex = 5;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1107, 12);
+            this.button1.Location = new System.Drawing.Point(1022, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(160, 23);
             this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
+            this.button1.Text = "Начать тестирование";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.BeginTest);
             // 
-            // label_id
+            // progressBar1
             // 
-            this.label_id.AutoSize = true;
-            this.label_id.Font = new System.Drawing.Font("Lucida Bright", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_id.Location = new System.Drawing.Point(6, 96);
-            this.label_id.Name = "label_id";
-            this.label_id.Size = new System.Drawing.Size(0, 17);
-            this.label_id.TabIndex = 3;
+            this.progressBar1.Location = new System.Drawing.Point(13, 584);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1169, 23);
+            this.progressBar1.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1192, 615);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.PortCB);
             this.Controls.Add(this.RateCB);
@@ -345,10 +159,6 @@
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -359,33 +169,13 @@
         private System.Windows.Forms.ComboBox RateCB;
         private System.Windows.Forms.ComboBox PortCB;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
-        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
-        private System.Windows.Forms.DataGridViewTextBoxColumn id;
-        private System.Windows.Forms.DataGridViewTextBoxColumn name;
-        private System.Windows.Forms.DataGridViewTextBoxColumn startValue;
-        private System.Windows.Forms.DataGridViewTextBoxColumn finishValue;
-        private System.Windows.Forms.DataGridViewTextBoxColumn chekValue;
-        private System.Windows.Forms.DataGridViewTextBoxColumn startValueS2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn finishValueS2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn checkValueS2;
         private System.Windows.Forms.Label label_stats_connect;
         private System.Windows.Forms.Label label_version;
         private System.Windows.Forms.Label label_firmware;
         private System.Windows.Forms.Label label_id;
+        private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
 
--- FW16AutoTestProgram/FW16AutoTestProgram/Form1.cs
+++ FW16AutoTestProgram/FW16AutoTestProgram/Form1.cs
@@ -17,6 +17,7 @@ namespace FW16AutoTestProgram
         public EcrCtrl ecrCtrl;                 //подключение к ККТ
         public int[] counters = new int[22];    //массив счётчиков
         public int[] registers = new int[236];  //массив регистров
+        string nameOerator = "test program";    //имя касира 
 
         public Form1()
         {
@@ -35,11 +36,7 @@ namespace FW16AutoTestProgram
             {
 
                 ecrCtrl.Init(serialPort, baudRate);             //Подключчение по порту и частоте
-                label_stats_connect.Text = "ККТ: подключено";
-                label_version.Text = "Версия прошивки :" + ecrCtrl.Info.FactoryInfo.FwBuild;
-                label_firmware.Text = "Код firmware:" + ecrCtrl.Info.FactoryInfo.FwType;
-                label_id.Text = "Серийный номер ККТ:" + ecrCtrl.Info.EcrInfo.Id;
-                //MessageBox.Show(Convert.ToString( ecrCtrl.Info.));
+                ShowInformation();
             }
             catch (EcrException excep)
             {
@@ -54,49 +51,30 @@ namespace FW16AutoTestProgram
             {
                 MessageBox.Show(excep.Message);                 //вывод ошибки занятости порта
             }
+
         }
         void ShowInformation()
         {
-
+                label_stats_connect.Text = "ККТ: подключено";
+                label_version.Text = "Версия прошивки :" + ecrCtrl.Info.FactoryInfo.FwBuild;
+                label_firmware.Text = "Код firmware:" + ecrCtrl.Info.FactoryInfo.FwType;
+                label_id.Text = "Серийный номер ККТ:" + ecrCtrl.Info.EcrInfo.Id;
+                //MessageBox.Show(Convert.ToString( ecrCtrl.Info.));
         }
 
-        void getCounterData()
+        private void BeginTest(object sender, EventArgs e)
+        { 
+            TestingClosedShift();
+        }
+
+        public void TestingClosedShift()
         {
-            dataGridView2.RowCount = 1;
-            for (ushort i = 1; i <= 22; i++)
+            if((ecrCtrl.Info.Status & Fw16.Ecr.GeneralStatus.ShiftOpened) > 0)
             {
-                try
-                {
-                    dataGridView2.Rows.Add(i, "", ecrCtrl.Info.GetCounter(i));  //Добавление строки в таблицу с данными счётчиков
-                }
-                catch { }
+                ecrCtrl.Shift.Close(nameOerator);
             }
-            dataGridView2.Rows.Remove(dataGridView2.Rows[0]);
-        }
-
-        void getRegisterrData()
-        {
-            dataGridView1.RowCount = 1;
-            for (ushort i = 1; i <= 236; i++)
-            {
-                try
-                {
-                    dataGridView1.Rows.Add(i, "", ecrCtrl.Info.GetRegister(i)); //добавление строки в тбалицу с данными регистров
-                }
-                catch { }
-            }
-            dataGridView1.Rows.Remove(dataGridView1.Rows[0]);
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            getCounterData();
-            getRegisterrData();
-        }
-
-        private void groupBox1_Enter(object sender, EventArgs e)
-        {
-
+            ecrCtrl.Shift.Close(nameOerator);
+            ecrCtrl.Shift.BeginCorrection(nameOerator, Fw16.Model.ReceiptKind.Income);
         }
     }
 }
