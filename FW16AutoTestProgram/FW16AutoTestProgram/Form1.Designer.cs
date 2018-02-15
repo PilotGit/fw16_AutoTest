9323f5fba7edb34c9c30ad490318d589db7dce3a
Schika <we19989@mail.ru>
2018-02-15 17:36:09 +0300

    мусор

--- FW16AutoTestProgram/FW16AutoTestProgram/Form1.Designer.cs
+++ FW16AutoTestProgram/FW16AutoTestProgram/Form1.Designer.cs
@@ -31,16 +31,7 @@
             this.Connect = new System.Windows.Forms.Button();
             this.RateCB = new System.Windows.Forms.ComboBox();
             this.PortCB = new System.Windows.Forms.ComboBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.modelL = new System.Windows.Forms.Label();
-            this.idL = new System.Windows.Forms.Label();
-            this.versionL = new System.Windows.Forms.Label();
-            this.firmwareL = new System.Windows.Forms.Label();
-            this.statsConnectL = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.LogTB = new System.Windows.Forms.TextBox();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Connect
@@ -69,114 +60,28 @@
             this.PortCB.Size = new System.Drawing.Size(121, 21);
             this.PortCB.TabIndex = 2;
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.modelL);
-            this.groupBox1.Controls.Add(this.idL);
-            this.groupBox1.Controls.Add(this.versionL);
-            this.groupBox1.Controls.Add(this.firmwareL);
-            this.groupBox1.Controls.Add(this.statsConnectL);
-            this.groupBox1.Location = new System.Drawing.Point(13, 66);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(270, 537);
-            this.groupBox1.TabIndex = 3;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Данные о ККТ";
-            // 
-            // modelL
-            // 
-            this.modelL.AutoSize = true;
-            this.modelL.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.modelL.Location = new System.Drawing.Point(6, 130);
-            this.modelL.Name = "modelL";
-            this.modelL.Size = new System.Drawing.Size(61, 16);
-            this.modelL.TabIndex = 4;
-            this.modelL.Text = "Модель:";
-            // 
-            // idL
-            // 
-            this.idL.AutoSize = true;
-            this.idL.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.idL.Location = new System.Drawing.Point(6, 101);
-            this.idL.Name = "idL";
-            this.idL.Size = new System.Drawing.Size(149, 16);
-            this.idL.TabIndex = 3;
-            this.idL.Text = "Серийный номер ККТ:";
-            // 
-            // versionL
-            // 
-            this.versionL.AutoSize = true;
-            this.versionL.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.versionL.Location = new System.Drawing.Point(6, 72);
-            this.versionL.Name = "versionL";
-            this.versionL.Size = new System.Drawing.Size(125, 16);
-            this.versionL.TabIndex = 2;
-            this.versionL.Text = "Версия прошивки:";
-            // 
-            // firmwareL
-            // 
-            this.firmwareL.AutoSize = true;
-            this.firmwareL.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.firmwareL.Location = new System.Drawing.Point(6, 43);
-            this.firmwareL.Name = "firmwareL";
-            this.firmwareL.Size = new System.Drawing.Size(88, 16);
-            this.firmwareL.TabIndex = 1;
-            this.firmwareL.Text = "Код firmware:";
-            // 
-            // statsConnectL
-            // 
-            this.statsConnectL.AutoSize = true;
-            this.statsConnectL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.statsConnectL.Location = new System.Drawing.Point(44, 16);
-            this.statsConnectL.Name = "statsConnectL";
-            this.statsConnectL.Size = new System.Drawing.Size(181, 20);
-            this.statsConnectL.TabIndex = 0;
-            this.statsConnectL.Text = "ККТ: не подключено";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1020, 27);
+            this.button1.Location = new System.Drawing.Point(282, 84);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Начать тестирование";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.BeginTest);
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(13, 584);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(1169, 23);
-            this.progressBar1.TabIndex = 7;
-            // 
-            // LogTB
-            // 
-            this.LogTB.Location = new System.Drawing.Point(288, 66);
-            this.LogTB.Multiline = true;
-            this.LogTB.Name = "LogTB";
-            this.LogTB.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.LogTB.Size = new System.Drawing.Size(892, 536);
-            this.LogTB.TabIndex = 8;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1192, 615);
-            this.Controls.Add(this.LogTB);
-            this.Controls.Add(this.progressBar1);
+            this.ClientSize = new System.Drawing.Size(394, 325);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.PortCB);
             this.Controls.Add(this.RateCB);
             this.Controls.Add(this.Connect);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -185,15 +90,7 @@
         private System.Windows.Forms.Button Connect;
         private System.Windows.Forms.ComboBox RateCB;
         private System.Windows.Forms.ComboBox PortCB;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label statsConnectL;
-        private System.Windows.Forms.Label versionL;
-        private System.Windows.Forms.Label firmwareL;
-        private System.Windows.Forms.Label idL;
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.TextBox LogTB;
-        private System.Windows.Forms.Label modelL;
     }
 }
 
--- FW16AutoTestProgram/FW16AutoTestProgram/Form1.cs
+++ FW16AutoTestProgram/FW16AutoTestProgram/Form1.cs
@@ -15,11 +15,6 @@ namespace FW16AutoTestProgram
     {
 
         public EcrCtrl ecrCtrl;                 //подключение к ККТ
-        public int[] counters = new int[22];    //массив счётчиков
-        public int[] registers = new int[236];  //массив регистров
-        string nameOerator = "test program";    //имя касира 
-        decimal[] coasts = new decimal[] { 200m, 200.37m };
-        decimal[] counts = new decimal[] { 1m, 5m, 0.39m, 1.73m };
 
         public Form1()
         {
@@ -37,7 +32,6 @@ namespace FW16AutoTestProgram
             try
             {
                 ecrCtrl.Init(serialPort, baudRate);             //Подключчение по порту и частоте
-                ShowInformation();
             }
             catch (EcrException excep)
             {
@@ -54,153 +48,10 @@ namespace FW16AutoTestProgram
             }
 
         }
-        void ShowInformation()
+
+        private void button1_Click(object sender, EventArgs e)
         {
-            statsConnectL.Text = "ККТ: подключено";
-            versionL.Text = "Версия прошивки: " + ecrCtrl.Info.FactoryInfo.FwBuild;
-            firmwareL.Text = "Код firmware: " + ecrCtrl.Info.FactoryInfo.FwType;
-            idL.Text = "Серийный номер ККТ: " + ecrCtrl.Info.EcrInfo.Id;
-            modelL.Text ="Модель: "+ecrCtrl.Info.EcrInfo.Model;
-            //MessageBox.Show(Convert.ToString( ecrCtrl.Info.));
-        }
-
-        private void BeginTest(object sender, EventArgs e)
-        {
-            Preparation();
-            SimpleTest();
-        }
-
-        public void Preparation()                                                                        //Функция подготовки к тестам
-        {
-            RequestRegisters();
-            RequestCounters();
-            ecrCtrl.Service.SetParameter(Native.CmdExecutor.ParameterCode.AbortDocFontSize, "51515");    //отключение печати чека
-            if ((ecrCtrl.Info.Status & Fw16.Ecr.GeneralStatus.DocOpened) > 0)
-            {
-                ecrCtrl.Service.AbortDoc();                                                             //закрыть документ если открыт
-            }
-            if ((ecrCtrl.Info.Status & Fw16.Ecr.GeneralStatus.ShiftOpened) > 0)
-            {
-                ecrCtrl.Shift.Close(nameOerator);                                                       //закрыть смену если открыта
-            }
-        }
-
-        public void SimpleTest()                            //функция прогона по всем видам чеков и чеков коррекции
-        {
-            ecrCtrl.Shift.Open(nameOerator);                //открытие смены для этого теста
-            TestReceipt();                                  //вызов функции тестирования чека
-            TestCorrection();                               //вызов функции тестирования чека коррекции
-            TestNonFiscal();                                //вызов функции нефискального документа
-            ecrCtrl.Shift.Close(nameOerator);               //закрытие смены этого теста
-
-            RequestRegisters();
-            RequestCounters();
-
-            LogTB.Text += "Завершено тестирование SimpleTest \r\n";     //логирование
-        }
-
-        private void TestNonFiscal()                                                                //тест нефискального документа
-        {
-            for (int nfdType = 1; nfdType < 4; nfdType++)                                           //Перебор типов нефиксальных документов
-            {
-                var document = ecrCtrl.Shift.BeginNonFiscal((Native.CmdExecutor.NFDocType)nfdType); //открытие нефиксального документа
-                for (int i = 0; i < 14 && nfdType < 3; i++)                                         //
-                {
-                    var tender = new Fw16.Model.Tender();
-                    tender.Amount = coasts[i / 7];
-                    tender.Code = (Native.CmdExecutor.TenderCode)(i % 7);
-                    document.AddTender(tender);
-                }
-                document.PrintText("Тестовый текст теста текстовго нефиксального документа");
-                document.Complete(Native.CmdExecutor.DocEndMode.Default);                                                                //закрытие нефиксального документа
-                LogTB.Text += "Оформлен нефиксальный документ типа " + (Native.CmdExecutor.NFDocType)nfdType + "\r\n";
-            }
-        }
-
-        private void TestCorrection()
-        {
-            for (int ReceptKind = 1; ReceptKind < 4; ReceptKind += 2)
-            {
-                var document = ecrCtrl.Shift.BeginCorrection(nameOerator, (Fw16.Model.ReceiptKind)ReceptKind);
-                decimal sum = 0;
-                for (int i = 0; i < 7; i++)                                                                         //перебор возврата средств всеми способами, целове и дробная суммы
-                {
-                    document.AddTender((Native.CmdExecutor.TenderCode)(i / 2), coasts[i % 2]);
-                    sum += coasts[i % 2];
-                }
-                for (int i = 0; i < 5; i++)                                                                         //перебор налоговых ставок
-                {
-                    document.AddAmount((Fw16.Model.VatCode)((i / 2) + 1), Math.Round(sum/6,2));
-                }
-                document.AddAmount(Fw16.Model.VatCode.NoVat, sum - Math.Round(sum / 6, 2) * 5);
-                document.Complete();                                                                                //закрытие чека корректировки
-                LogTB.Text += "Оформлен чек коррекции типа " + (Fw16.Model.ReceiptKind)ReceptKind + "\r\n";         //логирование
-            }
-        }
-
-        private void TestReceipt()
-        {
-            for (int ReceptKind = 1; ReceptKind < 5; ReceptKind++)
-            {
-                var document = ecrCtrl.Shift.BeginReceipt(nameOerator, (Fw16.Model.ReceiptKind)ReceptKind, new
-                {
-                    Taxation = Fs.Native.TaxationType.Agro,     //налогообложение по умолчанию
-                    CustomerAddress = Properties.Settings.Default.customEmail,        //адрес получателя
-                    SenderAddress = Properties.Settings.Default.senderEmail            //адрес отправтеля
-                });
-                Fw16.Ecr.ReceiptEntry receiptEntry;
-                for (int i = 0; i < 48; i++)
-                {
-                    //создание товара
-                    receiptEntry = document.NewItemCosted(i.ToString(), "tovar " + i, counts[i / 12], (Native.CmdExecutor.VatCodeType)((i / 2 % 6) + 1), coasts[i % 2]);
-                    document.AddEntry(receiptEntry);                                                //добавления товара в чек
-                    //textBox1.Text += "Добавлен " + "tovar " + i + "\r\n";
-                }
-                decimal balance = Math.Round(document.Total / 8, 2);                                //Сумма разделённая на количество типов оплаты.
-                for (int i = 7; i > 0; i--)
-                {
-                    Math.Round(document.AddPayment((Native.CmdExecutor.TenderCode)i, balance));     //оплата всеми способами кроме нала
-                }
-                balance = document.Total - document.TotalaPaid;                                     //вычисление остатка суммы для оплаты 
-                document.AddPayment((Native.CmdExecutor.TenderCode)0, balance);                     //оплата наличнми
-                RequestRegisters(160, 181);                                                         //запрос регистров по открытому документу
-                document.Complete();
-                LogTB.Text += "Оформлен чек типа " + (Fw16.Model.ReceiptKind)ReceptKind + "\r\n";     //логирование
-            }
-        }
-
-        public void RequestRegisters(ushort startIndex = 0, ushort endIndex = 0)      //запрос значений всех регистров / начиная с индекса / в диапозоне [startIndex,endIndex) 
-        {
-            endIndex = endIndex > 0 ? endIndex : Properties.Settings.Default.CountRegisters;
-            for (ushort i = startIndex; i < endIndex; i++)
-            {
-                try
-                {
-                    ecrCtrl.Info.GetRegister(i);
-                }
-                catch (Exception)
-                {
-                    LogTB.Text += "Не удолось получить доступ к регистру №" + i+"\r\n";
-                }
-            }
-            LogTB.Text += "Запрошены данные с регистров с " + startIndex + " по " + endIndex + "\r\n";     //логирование
-        }
-
-        public void RequestCounters(ushort startIndex = 1, ushort endIndex = 0)        //запрос значений всех счётчиков / начиная с индекса / в диапозоне [startIndex,endIndex)
-        {
-            endIndex = endIndex > 0 ? endIndex : Properties.Settings.Default.CountCounters;
-            for (ushort i = startIndex; i < endIndex; i++)
-            {
-                try
-                {
-                    ecrCtrl.Info.GetCounter(i);
-                }
-                catch (Exception)
-                {
-                    LogTB.Text += "Не удолось получить доступ к счётчику №" + i+"\r\n";
-                }
-            }
-            LogTB.Text += "Запрошены данные с счётчиков с " + startIndex + " по " + endIndex + "\r\n";     //логирование
+            ecrCtrl.Commands.BeginOfdRead().
         }
     }
 }
